a94336ae43595efcbbed09a5fef2e4bba54e4b37
TRichardVD <150446958+TRichardVD@users.noreply.github.com>
2024-09-13 08:55:24 +0200

    feat(Drones) : Application la technique de l'encapsulation

---
+++ personnel/Drones/Drones/Drones/Program.cs
@@ -0,0 +1,24 @@
+namespace Drones
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+
+            // Création de la flotte de drones
+            List<Drone> fleet= new List<Drone>();
+            Drone drone = new Drone("Joe", 100, 100);
+            fleet.Add(drone);
+
+            // Démarrage
+            Application.Run(new AirSpace(fleet));
+        }
+    }
+}
