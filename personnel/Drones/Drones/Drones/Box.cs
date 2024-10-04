d91dc3a3e7f3101e8c6a2d59733d3b1e4b687f6e
TRichardVD <150446958+TRichardVD@users.noreply.github.com>
2024-10-04 08:55:40 +0200

    feat(Drones) : Création de la classe Box

---
+++ personnel/Drones/Drones/Drones/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones
+{
+    internal class Box
+    {
+
+        
+
+
+
+
+
+    }
+}
