e46f5bb3ba9d776d7b2b8899a1ac28066fd9bd16
TRichardVD <150446958+TRichardVD@users.noreply.github.com>
2024-10-04 10:54:02 +0200

    feat(Box): Ajout des caractéristiques d'un paquet

--- personnel/Drones/Drones/Drones/Box.cs
+++ personnel/Drones/Drones/Drones/Box.cs
@@ -9,10 +9,22 @@ namespace Drones
 {
     internal class Box
     {
+        public readonly int id;
+        public int NbrKilos = 0;
+        public readonly Color SmartiesColor;
+        public readonly int IdCreator;
 
-        
+        private int NbrBoxes = 0;
 
+        public Box(int idCreator, int nbrkilos, Color SmartiesColor) 
+        {
+            this.SmartiesColor = SmartiesColor;
+            this.NbrKilos = nbrkilos;
+            this.IdCreator = idCreator;
 
+            this.id = NbrBoxes++;
+
+        }
 
 
 
