9932fa93f0e68756357882e58f6b180f002724d5
TRichardVD <150446958+TRichardVD@users.noreply.github.com>
2024-10-04 11:16:32 +0200

    feat(Drones): Création de l'interface `IDispatchable`

---
+++ personnel/Drones/Drones/Drones/Model/IDispatchable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones.Model
+{
+    interface IDispatchable
+    {
+        void WarnBox(Box box);
+
+        void RmvBox(Box box);
+    }
+}
